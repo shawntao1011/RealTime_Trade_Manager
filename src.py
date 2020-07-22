bfb823f3a2229ea811adc34ed13c4718153a604d
Tao <shawntao1011@gmail.com>
2020-07-22 11:01:20 +0800

    启用了 tab1的功能： 实时读取交易信息
    重写了tab2 线程测试用的代码 经测试，可以和tab1的读取进程共同运行
    
    暂时关闭了图片的更新进程，替换为输出进程
    
    后续准备改写tab2 图片更新进程 并替换输出进程:

--- src.py
+++ src.py
@@ -230,28 +230,45 @@ class MyAxisItem(pg.AxisItem):
             strings.append(vstr)
         return strings
 
+#### tab2 线程############################
+class Update_tab2(QtCore.QThread):
+    requestChange = QtCore.pyqtSignal(str)
+
+    def __init__(self, parent=None):
+        super(Update_tab2, self).__init__(parent)
+        # 设置工作状态与初始num数值
+
+    def __del__(self):
+        # 线程状态改变与线程终止
+        self.working = False
+        self.wait()
+
+    def run(self):
+        for i in range(20):
+            time.sleep(1)
+            # 通过自定义信号把待显示的字符串传递给槽函数
+            self.requestChange.emit(str(i))
+
+
 ## 主基类，是 整个GUI的主窗口，内部含有三个子窗口
 class Control_sys_Tab(QTabWidget):
     def __init__(self, parent=None):
-        #         self.RequestRowKey ={}
-        #         self.OrderRowKey  ={}
-        #         self.BatchRowKey  ={}   #To manage batch row index
-        #         self.ErrorRowKey  ={}   #To manage error row index
-        #         self.BuyBatchValue = {}
-        #         self.SellBatchValue= {}
-        #         self.BatchManagers  ={} # To manage value for each batch
-        #         self.g_CurrRequestRow = 0
-        #         self.g_CurrOrderRow  = 0
-        #         self.g_CurrBatchRow  = 0
-        #         self.g_CurrErrorRow  = 0
+        self.RequestRowKey ={}
+        self.OrderRowKey  ={}
+        self.BatchRowKey  ={}   #To manage batch row index
+        self.ErrorRowKey  ={}   #To manage error row index
+        self.BuyBatchValue = {}
+        self.SellBatchValue= {}
+        self.BatchManagers  ={} # To manage value for each batch
+        self.g_CurrRequestRow = 0
+        self.g_CurrOrderRow  = 0
+        self.g_CurrBatchRow  = 0
+        self.g_CurrErrorRow  = 0
         super().__init__(parent)
 
         self.setObjectName("Control_system")
         self.resize(1800, 985)
         self.setWindowTitle("实时监控系统")
-
-        # 记录tab2 中的数据，后续会从其他类中读取数据，并更新和作图
-        self.Data = []
 
         # 创建3个选项卡小控件窗口
         self.tab1 = QWidget()
@@ -263,11 +280,17 @@ class Control_sys_Tab(QTabWidget):
         self.addTab(self.tab2, "PNL展示界面")
         self.addTab(self.tab3, "PNL定时发送")
 
+        # 记录tab2 中的数据，后续会从其他类中读取数据，并更新和作图
+        self.Data=[]
+        self.work=Update_tab2()
+        self.execute()
+
         # 每个选项卡自定义的内容
         self.tab1UI()
         self.tab2UI()
         self.tab3UI()
 
+        self.pushButton.clicked.connect(self.slotStart)
     ################tab1#################################
 
     def tab1UI(self):
@@ -354,6 +377,20 @@ class Control_sys_Tab(QTabWidget):
         self.tab1.setLayout(layout)
 
     #################tab2#################################
+    ######################################
+    # 测试线程用代码
+    def execute(self):
+        # 启动线程
+        self.work.start()
+        # 线程自定义信号连接的槽函数
+        self.work.requestChange.connect(self.display)
+
+    def display(self, str):
+        # 由于自定义信号时自动传递一个字符串参数，所以在这个槽函数中要接受一个参数
+        print(str)
+
+
+    ########################################
     # 定义一个计时器
     def timer_start(self):
         self.timer = QtCore.QTimer(self)
@@ -392,14 +429,128 @@ class Control_sys_Tab(QTabWidget):
 
     def tab2UI(self):
         #         self.timer_start()
-        self.timer_start()
+        self.layout2 = QVBoxLayout()
+        #self.timer_start()
 
     def tab3UI(self):
 
         return
 
     ####################################################################################################################
+    ##############################################################
+    #     PyQt5 中的pyQtslot 是python中的decorator，用其可以将一个method 定义为 槽
 
+    #     槽的传参方式 主要是直接传入一个 函数指针
+
+    ##############################################################
+
+    @QtCore.pyqtSlot()
+    def slotStart(self):
+        # 按钮 暂停使用
+        self.pushButton.setEnabled(False)
+        # 开启一个新进程用来 更新数据
+        self.update_data_thread = UpdateData(self)
+        self.update_data_thread.requestChanged.connect(self.onRequestChanged)
+        # 线程进入 准备阶段
+        self.update_data_thread.start()
+
+    @QtCore.pyqtSlot(int, int, str)
+    def onRequestChanged(self, row, msgType, text):
+        elems = text.split(',')
+        # row =0
+        column = 0
+        if (msgType == 1):
+            myKey = self.RequestRowKey.get(elems[2] + elems[3])
+            if (myKey == None):
+                myKey = self.g_CurrRequestRow
+                self.g_CurrRequestRow += 1
+                self.RequestRowKey[elems[2] + elems[3]] = myKey  ### CREATE A REQUEST ROW
+
+            for ele in elems:
+                it = self.tableWidget1.item(myKey, column)
+                if it is None:
+                    it = QtWidgets.QTableWidgetItem()
+                    self.tableWidget1.setItem(myKey, column, it)
+                if (column == 0):
+                    it.setText(AcctNameDict[int(ele)])
+                    # print("What is this: %d, and acctName =  %s"%(int(ele),AcctNameDict[int(ele)]))
+                elif (column == 4):
+                    it.setText(directionType[ele])
+                else:
+                    it.setText(ele)
+                column += 1
+            self.tableWidget1.selectRow(myKey)
+
+            myKey = self.BatchRowKey.get(elems[2])
+            if (myKey == None):  ### UPDATE ACCT-BATCH TABLE HERE
+                myKey = self.g_CurrBatchRow
+                self.g_CurrBatchRow += 1
+                self.BatchRowKey[elems[2]] = myKey
+                self.BatchManagers[elems[2]] = BatchManager()  ### CREATE A BATCH MANAGER
+                self.BatchManagers[elems[2]].bookAcctID(int(elems[0]))
+            # print("what is the direction code: %s"%(elems[4]))
+            myTradeDirection = 1.0 if (int(elems[4]) == 0) else -1.0
+            self.BatchManagers[elems[2]].bookTotalValue(elems[3], float(elems[5]) * float(elems[10]) * myTradeDirection)
+            self.BatchManagers[elems[2]].bookTradedValue(elems[3], float(elems[8]))  ### need to change here
+            print(elems[3], float(elems[8]))
+
+            ### FILL INFORMATION IN THIS ROW
+            ###print("GET NOTIONA:",str(self.BatchManagers[elems[2]].getBuyNotional()),str(self.BatchManagers[elems[2]].getSellNotional()))
+            thisAcctID = self.BatchManagers[elems[2]].getAcctID()
+            thisAcctIDStr = AcctNameDict[thisAcctID]
+            vals1 = self.BatchManagers[elems[2]].getBuyNotional()
+            vals2 = self.BatchManagers[elems[2]].getSellNotional()
+            vals = [elems[2], thisAcctIDStr, "{:,.2f}".format(vals1[0]), "{:,.2f}".format(vals2[0]),
+                    "{:.2%}".format(vals1[1]), "{:.2%}".format(vals2[1])]
+            for column in range(0, 6):
+                it = self.tableWidget3.item(myKey, column)
+                if it is None:
+                    it = QtWidgets.QTableWidgetItem()
+                    self.tableWidget3.setItem(myKey, column, it)
+                it.setText(vals[column])
+            self.tableWidget3.selectRow(myKey)
+
+        elif (msgType == 2):
+            myKey = self.OrderRowKey.get(elems[0] + elems[1] + elems[12])
+            if (myKey == None):
+                myKey = self.g_CurrOrderRow
+                self.g_CurrOrderRow += 1
+                self.OrderRowKey[elems[0] + elems[1] + elems[12]] = myKey
+            for ele in elems:
+                it = self.tableWidget2.item(myKey, column)
+                if it is None:
+                    it = QtWidgets.QTableWidgetItem()
+                    self.tableWidget2.setItem(myKey, column, it)  # can be done in dictionary of dictionary way, better
+                if (column == 3):
+                    it.setText(directionType[ele])
+                elif (column == 4):
+                    it.setText(OpenCloseFlag[ele])
+                elif (column == 5):
+                    it.setText(hedgeFlag[ele])
+                elif (column == 11):
+                    it.setText(orderStatus[ele])
+                else:
+                    it.setText(ele)
+                column += 1
+            self.tableWidget2.selectRow(myKey)
+
+        elif (msgType == 3):
+            myKey = self.ErrorRowKey.get(elems[0] + elems[2])
+            if (myKey == None):
+                myKey = self.g_CurrErrorRow
+                self.g_CurrErrorRow += 1
+                self.ErrorRowKey[elems[0] + elems[2]] = myKey
+            for ele in elems:
+                it = self.tableWidget4.item(myKey, column)
+                if it is None:
+                    it = QtWidgets.QTableWidgetItem()
+                    self.tableWidget4.setItem(myKey, column, it)  # can be done in dictionary of dictionary way, better
+                it.setText(ele)
+                column += 1
+            self.tableWidget4.selectRow(myKey)
+        else:
+            self.tableWidget1.selectRow(row)
+#####################################################################################################################
 
 ################################################ main ##############################################################
 
